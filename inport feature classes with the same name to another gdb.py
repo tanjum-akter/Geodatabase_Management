fa8bcc81a3b7edf2d0c45597e80a1fa0bd7d8b52
Tanjum Akter <tanjum.iwm@gmail.com>
2026-01-20 19:33:39 +0600

    Add files via upload

---
+++ inport feature classes with the same name to another gdb.py
@@ -0,0 +1,22 @@
+import arcpy, os
+
+# Input and output geodatabases
+in_gdb  = r"D:\_TANJUM\_GitHub\Import_MG_to_GDB\Bhairab_Bazar.gdb"
+out_gdb = r"D:\_TANJUM\_GitHub\Import_MG_to_GDB\Bhairab_Bazar_MG.gdb"
+
+# Set workspace
+arcpy.env.workspace = in_gdb
+
+# Create output GDB if it does not exist
+if not arcpy.Exists(out_gdb):
+    arcpy.CreateFileGDB_management(
+        os.path.dirname(out_gdb),
+        os.path.basename(out_gdb)
+    )
+
+# Copy feature classes ending with _MG
+for fc in arcpy.ListFeatureClasses("*_MG"):
+    arcpy.CopyFeatures_management(fc, os.path.join(out_gdb, fc))
+    print("Copied: {0}".format(fc.encode("utf-8")))
+
+print("Done.")
