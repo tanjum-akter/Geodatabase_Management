ad3e19db0c02ebd2bb8d58512f2a172071301944
Tanjum Akter <tanjum.iwm@gmail.com>
2026-01-22 13:46:46 +0600

    Count features in a feature class.py

--- Count features in a feature class.py
+++ Count features in a feature class.py
@@ -1,9 +1,10 @@
 import arcpy
 
-arcpy.env.workspace = r"D:\_TANJUM\Count features in a feature class.py\Bhairab_Bazar.gdb"
+arcpy.env.workspace = r"D:\_TANJUM\Count features in a feature class\Bhairab_Bazar.gdb"
 
 for fc in arcpy.ListFeatureClasses():
     count = int(arcpy.GetCount_management(fc)[0])
     print("{} {}".format(fc, count))
+
 
 
