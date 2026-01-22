fdc90ed6e7b9daa26a35cc04ab030367e6130c57
Tanjum Akter <tanjum.iwm@gmail.com>
2026-01-22 13:33:36 +0600

    Count total feature classes and list.py

--- Count total feature classes and list.py
+++ Count total feature classes and list.py
@@ -1,11 +1,10 @@
 import arcpy
 
-gdb = r"D:\_TANJUM\Count total feature classes and list\Bhairab_Bazar.gdb"
-arcpy.env.workspace = gdb
+arcpy.env.workspace = r"D:\_TANJUM\ Count total feature classes and list\Bhairab_Bazar.gdb"
 
-fc_list = arcpy.ListFeatureClasses() or []
-for fd in arcpy.ListDatasets(feature_type='Feature') or []:
-    fc_list += arcpy.ListFeatureClasses(feature_dataset=fd) or []
+fc_list = arcpy.ListFeatureClasses()
 
-print("Total feature classes:", len(fc_list))
-print("\nFeature classes list:\n" + "\n".join(fc_list))
+print("Total number of feature classes: {}".format(len(fc_list)))
+print("\nFeature classes:")
+for fc in fc_list:
+    print(fc)
