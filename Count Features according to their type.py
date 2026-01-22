b3732a16839a59261b7bb3a9341b746d3bc16702
Tanjum Akter <tanjum.iwm@gmail.com>
2026-01-22 12:15:29 +0600

    Count Features according to their type.py

--- Count Features according to their type.py
+++ Count Features according to their type.py
@@ -1,5 +1,5 @@
 import arcpy
-arcpy.env.workspace = r"D:\_TANJUM\Layer Count According to Type\Bhairab_Bazar.gdb"
+arcpy.env.workspace = r"D:\_TANJUM\Count Features according to their type\Bhairab_Bazar.gdb"
 counts = {"Point":0,"Polyline":0,"Polygon":0}
 for fc in arcpy.ListFeatureClasses():
     t = arcpy.Describe(fc).shapeType
@@ -7,3 +7,4 @@ for fc in arcpy.ListFeatureClasses():
 
 for k,v in counts.items():
     print("{}: {}".format(k, v))
+
